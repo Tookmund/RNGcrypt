9197ffbfcab9f8c8010faf2b079cc55cc738f56c
Jacob Adams <jacobaa1031@gmail.com>
2014-10-19 00:16:22 -0400

    retab python file and give it all features of c version

--- RNGcrypt.py
+++ RNGcrypt.py
@@ -1,30 +1,31 @@
 #!/usr/bin/python
 import string
 import random
+import sys
 
 def randomdata(size):
-  randstr = ''
-  for x in range(size):
-    randstr.join(hex(random.randrange(2147483647)))
-  return randstr
-  
-def encrypt(string):
-  return randomdata(1024)
+	randstr = ''
+	for x in range(size):
+	randstr += hex(random.randrange(2147483647))
+	return randstr
+	
+def encrypt():
+	return randomdata(1024)
 
-# ajust comments if using python 3
-def p(string):
-  print string
-  #print(string)
+# adjust comments if using python 3
+def p(strng):
+	print strng
+	#print(string)
 
 if __name__ == "__main__":
-  random.seed()
-  inp = raw_input("Raw Text or File (t/f):")
-  if (inp == "t"): 
-    p(encrypt(raw_input("Enter Raw Text: ")))
-  elif (inp == "f"):
-    try:
-      fp = open(raw_input("Enter File Name: "))
-    except:
-      p("File failed to open")
-    else:
-      p(encrypt(fp.read()))
+	random.seed()
+	inp = raw_input("Raw Text or File (t/f):")
+	if (inp == "t"): 
+		p(encrypt(raw_input("Enter Raw Text: ")))
+	elif (inp == "f"):
+		try:
+			sys.stdout = open(raw_input("Enter File Name: ")+".rngc","r+")
+		except:
+			p("File failed to open")
+		else:
+			p(encrypt())
