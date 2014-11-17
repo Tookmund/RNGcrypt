edef434090d9cf18b97cadc25be6f5a615147bf1
Jacob Adams <Tookmund@users.noreply.github.com>
2014-11-17 09:41:03 -0500

    Create RNGcrypt.js

---
+++ RNGcrypt.js
@@ -0,0 +1,19 @@
+function decimalToHexString(number)
+{
+    if (number < 0)
+    {
+    	number = 0xFFFFFFFF + number + 1;
+    }
+
+    return number.toString(16).toUpperCase();
+}
+function rngcrypt (str) {
+  var ar = [];
+  for (var i=0;i<str.length;i++) {
+    ar[i] = Math.random();
+  }
+  for (var j=0;j<ar.length;i++) {
+    console.log(decimalToHexString(ar[j]));
+  }
+}
+console.read(rngcrypt);
